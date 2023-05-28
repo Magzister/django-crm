b0cc26ef999f3e8594e6e1b53dbcc9f27fbe8aff
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2023-05-28 11:26:53 +0300

    Add registration

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -16,7 +17,25 @@ def home(request):
         return redirect('home')
     return render(request, 'home.html', {})
 
+
 def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out!')
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have registered!")
+            return redirect('home')
+        else:
+            return render(request, 'register.html', {'form':form})
+    form = SignUpForm()
+    return render(request, 'register.html', {'form':form})
